0d3c762d3950d7cc01df0ac87136db62aa143fcd
pedrodruviaro <pedrodruviaro@gmail.com>
2023-05-22 20:02:26 -0300

    project finished

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,24 @@
-'use client';
+'use client'
+
+import styles from '../styles'
+import { motion } from 'framer-motion'
+import { navVariants } from '../utils/motion'
 
 const Navbar = () => (
-  <nav>
-    navbar
-  </nav>
-);
+    <motion.nav
+        variants={navVariants}
+        initial="hidden"
+        whileInView="show"
+        className={`${styles.xPaddings} py-8 relative`}
+    >
+        <div className="absolute w-[50%] inset-0 gradient-01" />
 
-export default Navbar;
+        <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+            <img src="/search.svg" alt="Search" className="w-[24px] h-[24px] object-contain" />
+            <h2 className="font-extrabold text-[24px] leading-[30px] text-white uppercase">Metaversus</h2>
+            <img src="/menu.svg" alt="menu" className="w-[24px] h-[24px] object-contain" />
+        </div>
+    </motion.nav>
+)
+
+export default Navbar
